a29bc92bf919825a0a48ce4032599372742dc8fa
Rehno Lindeque <errantkid@gmail.com>
2011-11-01 23:51:12 +0200

    Added a copyright (or rather anti-copyright) message javascript strictness annotation header file

---
+++ src/header.js
@@ -0,0 +1,7 @@
+/*
+ * glQuery
+ * Originaly copyright glQuery authors 2011, but is released into the public domain.
+ * (Also available under an MIT license and a GPL v2.0 license)
+ */
+"use strict";
+
