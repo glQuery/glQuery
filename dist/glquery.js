2da65f00a2a94af224a843e51ea6bf0a7dd2d3ea
Rehno Lindeque <errantkid@gmail.com>
2011-11-02 01:15:21 +0200

    Added a canvas method (for initializing a html canvas with a webgl context) Fixed logging methods Added run-time checking (and type checking... see also issue #3)

--- dist/glquery.js
+++ dist/glquery.js
@@ -15,12 +15,23 @@ var glQuery = (function() {
   // The scene which contains the hierarchy of identifiers
   scene = {},
   // Buckets that hold renderer state
-  buckets = {};
-
-  logInfo = function(msg) { console.log(msg); };
-  logWarning = function(msg) { console.log(msg); };
-  logError = function(msg) { console.log(msg); };
-  assert = function(condition, msg) { if (!condition) logError(msg); }
+  buckets = {},
+  // Logging / information methods
+  logInfo = function(msg) { console.log(msg); },
+  logWarning = function(msg) { console.log(msg); },
+  logError = function(msg) { console.log(msg); },
+  // Run-time checks
+  assert = function(condition, msg) { if (!condition) logError(msg); },
+  assertType = function(param, typeStr, parentFunction, paramStr) {
+    if (paramStr != null && parentFunction != null)
+      assert(typeof param === typeStr, "In call to '" + parentFunction + "', expected type '" + typeStr + "' for '" + paramStr + "'. Instead, got type '" + typeof param + "'.");
+    else if (parentFunction != null)
+      assert(typeof param === typeStr, "In call to '" + parentFunction + "', expected type '" + typeStr + "'. Instead, got type '" + typeof param + "'.");
+    else if (paramStr != null)
+      assert(typeof param === typeStr, "Expected type '" + typeStr + "' for '" + paramStr + "'. Instead, got type '" + typeof param + "'.");
+    else
+      assert(typeof param === typeStr, "Expected type '" + typeStr + "'. Instead, got type '" + typeof param + "'.");
+  };
 
   // glQuery API
   glQuery.fn = glQuery.prototype = {
@@ -29,7 +40,7 @@ var glQuery = (function() {
     },
     scene: function(sceneDef) {
       logInfo("scene");
-      if (typeof sceneDef === 'String') {
+      if (typeof sceneDef === 'string') {
         // TODO...
       }
     },
@@ -52,6 +63,11 @@ var glQuery = (function() {
       logInfo("light");
     },
     length: 0,
+  };
+
+  glQuery.canvas = function(htmlId) {
+    assertType(htmlId, 'string', 'canvas', 'htmlId');
+    logInfo("canvas: " + htmlId);
   };
 
   return glQuery;
