3b7835b8e2c9e47f61c083efbe87b8d69c56ee6f
Rehno Lindeque <errantkid@gmail.com>
2011-12-01 15:32:17 +0200

    Simplified the dummy api dispatch (in order to keep it consistent with changes to the api)

--- src/api.js
+++ src/api.js
@@ -1,18 +1,6 @@
   // glQuery API
   // Note: According to https://developer.mozilla.org/en/JavaScript/Reference/Functions_and_function_scope/arguments
   //       arguments is not a proper Array object, so assume arguments.slice is not implemented.
-  var apiDummy = {
-    init: function() { return this; },
-    render: function() { return this; },
-    command: function() { return this; },
-    shaderProgram: function() { return this; },
-    triangles: function() { return this; },
-    indices: function() { return this; },
-    vertices: function() { return this; },
-    material: function() { return this; },
-    light: function() { return this; }
-  };
-
   gl.fn = gl.prototype = {
     init: function() {
       //logDebug("init");
@@ -67,5 +55,10 @@
       commands.push(command.light, this._selector, Array.prototype.slice.call(arguments));
       return this;
     }
+  };
+
+  var apiDummy = {};
+  for (var key in gl.fn) {
+    apiDummy[key] = function() { return this; };
   };
 
