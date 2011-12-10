fb0c4ad613a90c2bd74b35dd48c86e8ba6d76c44
Rehno Lindeque <errantkid@gmail.com>
2011-12-10 15:11:14 +0200

    Added some extra checks to the lint script

--- lint.sh
+++ lint.sh
@@ -2,15 +2,21 @@
 
 rm src/*~ -f
 
+# Find all TODO's in the source code
+grep "TODO" src/* -r --color
+
 # x == [] is always false since javascript is comparing references rather than values
 # Use x.length == 0 instead
-grep "\=\= \[\]" src/* -r
-grep "\!\= \[\]" src/* -r
+grep "\=\= \[\]" src/* -r --color
+grep "\!\= \[\]" src/* -r --color
 
 # Search for debugging helpers
-grep "console.log" src/* -r
-grep "alert" src/* -r
+grep "console.log" src/* -r --color
+grep "alert" src/* -r --color
 
 # slice(0) can be written slice()
-grep "slice(0)" src/* -r
+grep "slice(0)" src/* -r --color
+
+# Properly capitalize the name 'WebGL'
+grep "webgl" src/* -r --color
 
