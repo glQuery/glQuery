838531ee7afad5edaaf3f8db7c091b2087f3e635
Rehno Lindeque <errantkid@gmail.com>
2011-12-10 10:34:55 +0200

    Added back the executable flag for lint and search for slice(0) where it occurs

--- lint.sh
+++ lint.sh
@@ -1,4 +1,6 @@
-# This script is helpful for checking 
+# This script is helpful for checking common javascript issue
+
+rm src/*~ -f
 
 # x == [] is always false since javascript is comparing references rather than values
 # Use x.length == 0 instead
@@ -8,3 +10,7 @@ grep "\!\= \[\]" src/* -r
 # Search for debugging helpers
 grep "console.log" src/* -r
 grep "alert" src/* -r
+
+# slice(0) can be written slice()
+grep "slice(0)" src/* -r
+
