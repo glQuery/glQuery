359231b0195b8b1b864ac4665d74016170aa9b7e
Rehno Lindeque <errantkid@gmail.com>
2011-12-15 17:11:25 +0200

    Use the strict directive inside the library closure to avoid polluting the global closure (may be helpful if the user joins the js file together with other js files that are not necessarily strict themselves)

--- src/header.js
+++ src/header.js
@@ -3,5 +3,5 @@
  * glQuery is free, public domain software (http://creativecommons.org/publicdomain/zero/1.0/)
  * Originally created by Rehno Lindeque of http://www.mischievousmeerkat.com
  */
-"use strict";
 var glQuery = (function() {
+"use strict";
