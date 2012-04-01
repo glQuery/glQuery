ac3f7a2c637350976f34eddde53941fca40a737d
Rehno Lindeque <errantkid@gmail.com>
2012-04-01 11:48:06 -0700

    Ensure that the same eventhandler cannot be added twice and add some typechecking

--- src/contextevents.js
+++ src/contextevents.js
@@ -1,11 +1,33 @@
   gl.contextlost = function(callback) {
-    if(callback == null)
+    var i;
+    // Clear the list of callbacks if nothing was passed in
+    if(typeof callback === 'undefined') {
       eventCallbacks.contextlost = [];
+      return;
+    }
+    // Check that callback is a function
+    assertType(callback, 'function', 'contextlost', 'callback');
+    // Prevent the same callback from being added to the list twice.
+    for (i = 0; i < eventCallbacks.contextlost.length; ++i)
+      if (eventCallbacks.contextlost[i] === callback)
+        return;
+    // Add the callback
     eventCallbacks.contextlost.push(callback);
   };
   gl.contextrestored = function(callback) {
-    if(callback == null)
+    var i;
+    // Clear the list of callbacks if nothing was passed in
+    if(typeof callback === 'undefined') {
       eventCallbacks.contextrestored = [];
+      return;
+    }
+    // Check that callback is a function
+    assertType(callback, 'function', 'contextrestored', 'callback');
+    // Prevent the same callback from being added to the list twice.
+    for (i = 0; i < eventCallbacks.contextrestored.length; ++i)
+      if (eventCallbacks.contextrestored[i] === callback)
+        return;
+    // Add the callback
     eventCallbacks.contextrestored.push(callback);
   };
 
